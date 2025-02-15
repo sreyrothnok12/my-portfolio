c45ce8bd9543f308d6df165e19be2f826e1e1064
sreyrothnok <noksreyroth@gmail.com>
2025-02-15 14:54:38 +0700

    Update homepage and base path configurations

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// No need to import '@tailwindcss/vite', just install and configure tailwindcss.
+import tailwindcss from 'tailwindcss' // This is just to highlight it's in use.
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/my-portfolio/', // Base path for GitHub Pages
 })
